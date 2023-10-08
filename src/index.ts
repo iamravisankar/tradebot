8ba6b22c4c9533997745fbf0641805434b89ccfd
Ravisankar Sivaji <ravi.sankar@aquera.com>
2023-10-09 03:26:48 +0530

    added stats

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
 import express from 'express';
+import { pool } from "./dbConfig";
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('durrrrr....');
+async function getTableCounts() {
+    const tables = ['stock_data', 'stock_depth_buy', 'stock_depth_sell', 'tech_analysis'];
+    const counts: Record<string, number> = {};
+
+    for (const table of tables) {
+        const res = await pool.query(`SELECT COUNT(*) FROM ${table}`);
+        counts[table] = parseInt(res.rows[0].count, 10);
+    }
+
+    return counts;
+}
+
+app.get('/', async (req, res) => {
+    try {
+        const counts = await getTableCounts();
+        res.json(counts);
+    } catch (error: any) {
+        console.error('Error getting table counts:', error);
+        res.status(500).send('Error retrieving data');
+    }
 });
 
 app.listen(PORT, () => {
