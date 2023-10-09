ca83d228d26558bc85bc8d483e01ba318b220c25
Ravisankar Sivaji <ravi.sankar@aquera.com>
2023-10-09 20:03:21 +0530

    main update

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,49 @@
-// Imports
 import { fetchDataAndInsert } from './fetchPaytmData';
 import { srAnalysis } from './srAnalysis';
 import { techAnalysis } from './techAnalysis';
+import { pool } from './dbConfig';
 
-// SR Analysis Function
+
+const fetchExchangeSymbolsFromDB = async (): Promise<string[]> => {
+    try {
+        const result = await pool.query('SELECT exchange, symbol FROM equity_security_master WHERE in_watchlist = true');
+        const exchangeSymbols: string[] = result.rows.map(row => `${row.exchange}_${row.symbol}`);
+        return exchangeSymbols;
+    } catch (error: any) {
+        console.error('Error fetching exchange symbols:', error.message);
+        return [];
+    }
+};
+
 async function fetchSrAnalysisData() {
     try {
-        const data = await srAnalysis("min", ["NSE_TATAMOTORS"]);
+        const exchangeSymbols: string[] = await fetchExchangeSymbolsFromDB();
+
+        if (exchangeSymbols.length === 0) {
+            console.log('No exchange symbols found in the watchlist.');
+            return;
+        }
+
+        const data = await srAnalysis("min", exchangeSymbols);
         // console.log('Fetched SR Analysis data:', data);
+
     } catch (error: any) {
         console.error('Failed to fetch SR Analysis data:', error.message);
     }
 }
 
-// Technical Analysis Function
 async function fetchTechAnalysisData() {
     try {
-        const data = await techAnalysis("min", ["NSE_TATAMOTORS"]);
+        const exchangeSymbols: string[] = await fetchExchangeSymbolsFromDB();
+
+        if (exchangeSymbols.length === 0) {
+            console.log('No exchange symbols found in the watchlist.');
+            return;
+        }
+
+        const data = await techAnalysis("min", exchangeSymbols);
         // console.log('Fetched Technical Analysis data:', data); // Uncomment if you want to log this data
+
     } catch (error: any) {
         console.error('Failed to fetch Technical Analysis data:', error.message);
     }
