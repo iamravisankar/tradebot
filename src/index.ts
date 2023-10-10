25ad9cc810ca5a89e65d8184faeedc254bccf46e
Ravisankar Sivaji <ravi.sankar@aquera.com>
2023-10-10 10:08:49 +0530

    ui

--- src/index.ts
+++ src/index.ts
@@ -19,12 +19,50 @@ async function getTableCounts() {
 app.get('/', async (req, res) => {
     try {
         const counts = await getTableCounts();
-        res.json(counts);
-    } catch (error: any) {
+
+        // Create HTML response with inline styles and scripts
+        const htmlResponse = `
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Table Counts</title>
+                <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+                <script src="https://cdn.jsdelivr.net/npm/animejs@3.2.1/lib/anime.min.js"></script>
+            </head>
+            <body>
+                <div class="container mt-5">
+                    <h2 class="text-center mb-4">Table Counts</h2>
+                    <div id="tableCounts" class="d-flex justify-content-center flex-wrap">
+                        ${Object.entries(counts).map(([table, count]) => `
+                            <div class="m-2 p-4 border rounded w-100 w-md-auto">
+                                <strong>${table}</strong>: <span class="count" data-count="${count}">0</span>
+                            </div>
+                        `).join('')}
+                    </div>
+                </div>
+
+                <script>
+                    anime({
+                        targets: '.count',
+                        innerHTML: [0, function(el) { return el.getAttribute('data-count'); }],
+                        round: 1,
+                        delay: anime.stagger(200),
+                        easing: 'easeInOutExpo'
+                    });
+                </script>
+            </body>
+            </html>
+        `;
+
+        res.send(htmlResponse);
+    } catch (error) {
         console.error('Error getting table counts:', error);
         res.status(500).send('Error retrieving data');
     }
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
